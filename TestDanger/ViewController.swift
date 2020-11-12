75bd79c59fafa966e6e2e25932d5040ef3abefb8
bfrolicher <bfrolicher@inflexsys.com>
2020-11-12 16:03:50 +0100

    Add viewModel to ViewController

--- TestDanger/ViewController.swift
+++ TestDanger/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var viewModel: ViewModelProtocol = ViewModel()
+    
+    @IBOutlet private weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.nameLabel.text = viewModel.name
     }
-
-
 }
 
